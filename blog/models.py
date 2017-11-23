b0149dbaea0b45917fbd0f57a30b2b42c8ff2487
pythonbyte <dudumelgaco@hotmail.com>
2017-11-23 21:20:32 -0200

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
 			blank=True, null=True)
 
 	def publish(self):
-		self.piblished_date = timezone.now
+		self.published_date = timezone.now()
 		self.save()
 
 	def __str__(self):
